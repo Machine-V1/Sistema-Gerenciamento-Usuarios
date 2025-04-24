0ed275c85af9830a9fc200758b43328edd75894a
Ivan Ramos <ivan.ramos0909.22@gmail.com>
2025-04-24 16:52:13 -0300

    Done 👌

--- script.js
+++ script.js
@@ -1,32 +1,72 @@
 let btnEnviar = document.getElementById("btn-enviar");
 let btnConsulta = document.getElementById("btn-consulta");
+// Criando Classe Usuario
 class Usuario {
   constructor(nome, senha) {
     this.nome = nome;
     this.senha = senha;
   }
   consultarDados() {
+    // retornando nome e senha em um objeto
     return { nome: this.nome, senha: this.senha };
   }
+  mudarSenha(senhaAlt) {
+    this.senha = senhaAlt;
+  }
 }
-let usuarioInstancia = [];
+let usuarioInstancia = []; // criando um array pra receber todos os Usuarios
 btnEnviar.addEventListener("click", (event) => {
   event.preventDefault();
+  // pegando o valor do nome e senha do input
   let nome = document.getElementById("input-nome").value;
   let senha = document.getElementById("input-senha").value;
+  document.getElementById("input-nome").value = "";
+  document.getElementById("input-senha").value = "";
+  // Colocando na Array
   usuarioInstancia.push(new Usuario(nome, senha));
 });
-listaHtmlElement = document.getElementById("lista-consulta");
+// pegando elemento HTML do comeco da lista
+let listaHtmlElement = document.getElementById("lista-consulta");
+
 btnConsulta.addEventListener("click", () => {
+  // para cada usuario
+
+  // Deletando todos os filhos pra nao repetir a consulta
+  while (listaHtmlElement.firstChild) {
+    listaHtmlElement.firstChild.remove();
+  }
+
   usuarioInstancia.forEach((usuario, index) => {
     console.log(usuario.consultarDados());
+
+    // Desconstruindo o objeto do retorno no metodo
     let { nome, senha } = usuario.consultarDados();
+    // Criando uma linha
+
     let linha = document.createElement("li");
+
     linha.appendChild(
-      document.createTextNode(`ID: ${index} Nome: ${nome} Senha: ${senha}`)
+      // preenchendo os parametros dentro do linha
+      document.createTextNode(`ID: ${index} || Nome: ${nome} || Senha: ${senha}`)
     );
+    // colocando dentro do ul
     listaHtmlElement.appendChild(linha);
   });
-  console.log(usuarioInstancia);
-  // ´´
+});
+
+let btnMudarSenha = document.getElementById("btn-mudar-senha");
+
+btnMudarSenha.addEventListener("click", (event) => {
+  event.preventDefault();
+  let confNome = document.getElementById("confirmar-nome").value;
+  let confSenha = document.getElementById("confirmar-senha").value;
+  let inputMudarSenha = document.getElementById("input-mudar-senha").value;
+
+  let usuario = usuarioInstancia.find(x => x.nome == confNome && x.senha == confSenha);
+  if(usuario){
+    usuario.mudarSenha(inputMudarSenha)
+
+  }else{
+    window.alert("Dados Incorretos")
+  }
 });
